9f6e4fa8303af38cbfca7c6260df00e4a61f1e92
PingWang <ping.wang@flexport.com>
2022-05-10 15:22:36 +0800

    add api

--- index.js
+++ index.js
@@ -7,6 +7,14 @@ const { init: initDB, Counter } = require("./db");
 const logger = morgan("tiny");
 
 const app = express();
+const scheduleData = require("./hackmini.json")
+const formatedScheduleData = scheduleData.map(row => {
+    return {
+        ...row,
+        etd: row["etd"] ? new Date(row["etd"]) : null
+    }
+})
+
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
@@ -37,6 +45,30 @@ app.get("/api/schedule_data", async (req, res) => {
     res.send({
         code: 0,
         data: "hello world"
+    })
+})
+
+app.get("/api/test", async (req, res) => {
+    res.send({
+        code: 0,
+        data: `${JSON.stringify(req.body)}`
+    })
+})
+
+app.get("/api/schedule", async (req, res) => {
+    const { pol, pod, etd_start, etd_end } = req.query;
+    if(!pol || !pod || !etd_start || !etd_end) {
+        res.send({
+            code: 1,
+            data: `One of pol, pod, etd_start, etd_end is missing. ${pol} ${pod} ${etd_start} ${etd_end}`
+        })
+    }
+    const etd_start_date = new Date(etd_start)
+    const etd_end_date = new Date(etd_end)
+    const result = formatedScheduleData.filter((row) => row.ports_of_loading == pol && (row.ports_of_discharge || []).includes(pod) && row.etd <= etd_end_date && row.etd >= etd_start_date)
+    res.send({
+        code: 0,
+        data: result
     })
 })
 
